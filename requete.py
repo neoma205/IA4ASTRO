452b926c51fbccf1e54fe4d871b586f121074614
neoma205 <neoma.essayan@yahoo.fr>
2024-06-18 13:56:10 +0200

    ajout fonction classe objets

--- requete.py
+++ requete.py
@@ -17,6 +17,26 @@ def get_one_object(name,enregistrer):
 
     return pdf
 
+
+def get_class_objet(classe_name,nombre,enregistrer):
+    r = requests.post(
+        'https://fink-portal.org/api/v1/latests',
+        json={
+            'class': classe_name,
+            'n': nombre,
+            'output-format': 'json'
+        }
+    )
+    pdf = pd.read_json(io.BytesIO(r.content))
+    
+    if enregistrer:
+        pdf.to_json(classe_name)
+    return pdf
+
+
+
+
+
 if __name__ == "__main__":
     pdf_1 = get_one_object("ZTF24aaqxcpz", True)
 
@@ -26,4 +46,9 @@ if __name__ == "__main__":
     pdf_2 = get_one_object("ZTF24aaqainu", True)
 
     print(pdf_2[["i:objectId", "i:magpsf", "i:jd", "i:ra", "i:dec"]])
+    
+    print()
+    pdf_3 = get_class_objet("SN candidate", 100, True)
+
+    print(pdf_3[["i:objectId", "i:magpsf", "i:jd", "i:ra", "i:dec", "d:cdsxmatch", "v:lapse"]])
     
