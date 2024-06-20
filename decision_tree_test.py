65c34cdc50d6a8ef7558d66f806d3c200f8bf1da
SamSamSamu <samueldibello@outlook.fr>
2024-06-20 09:38:45 +0200

    Base de donnée dans le dataset et decision tree fonctionnel

--- decision_tree_test.py
+++ decision_tree_test.py
@@ -5,6 +5,7 @@ import pandas as pd
 import io
 import random
 import json
+from sklearn.ensemble import RandomForestClassifier
 
 def creer_une_liste(pdf):
     pdf_utile = pdf["lc_features_r"]
@@ -16,17 +17,22 @@ def creer_une_liste(pdf):
             if not None in info:           
                 x.append(info)
     
-    return x, [classe for r in range(len(x))]
+    return x[:int(len(x)*0.7)], [classe for r in range(int(len(x)*0.7))], x[int(len(x)*0.7):], [classe for r in range(len(x[int(len(x)*0.7):]))]
 
 def dataset_sam(names):
     X=[]
     Y=[]
+    tx=[]
+    ty=[]
     for name in names: 
         pdf = pd.read_parquet(name).reset_index(drop=True)
-        a, b = creer_une_liste(pdf)
+        a, b, t_value, t_etiquettes = creer_une_liste(pdf)
+        
         X+=a
         Y+=b
-    return (X,Y)
+        tx+= t_value
+        ty+=t_etiquettes
+    return (X,Y), (tx, ty)
 
 def tester(tree, x, y):
     v=0
@@ -37,9 +43,13 @@ def tester(tree, x, y):
         t+=1
     return v/t
 
+
+def entrainer_algo(algo, data, etiquette):
+    return algo.fit(data, etiquette)
+
+
 if __name__ == "__main__":
-    clf = tree.DecisionTreeClassifier()
-    datas=dataset_sam(("dataset/dataset_EB__Candidate.parquet",
+    datas, data_test=dataset_sam(("dataset/dataset_EB__Candidate.parquet",
                  "dataset/dataset_EB_.parquet",
                  "dataset/dataset_LP__Candidate.parquet",
                  "dataset/dataset_LPV_.parquet",
@@ -53,11 +63,28 @@ if __name__ == "__main__":
         
     print(np.unique(datas[1][:185000], return_counts=True))
     print(np.unique(datas[1][185001:], return_counts=True))
-    exit()
-    clf = clf.fit(datas[0][:185000], datas[1][:185000])
     
-    print(tester(clf, datas[0][:185000], datas[1][:185000]))
-    print(tester(clf, datas[0][185001:], datas[1][185001:]))
+    clf = tree.DecisionTreeClassifier()
+    clf = entrainer_algo(clf, datas[0], datas[1])
+    print("test sur les données d'entrainement")
+    print(tester(clf, datas[0], datas[1]))
+    print(tester(clf, data_test[0], data_test[1]))
+    
+    
+    # #clf = clf.fit(datas[0][:185000], datas[1][:185000])
+    
+    # print(tester(clf, datas[0][:185000], datas[1][:185000]))
+    # print(tester(clf, datas[0], datas[1]))
+    
+    exit()
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    
+    print("demarrage de l'entrainement: ")
+    clf = entrainer_algo(clf, datas[0][:185000], datas[1][:185000])
+    print("fin de l'entrainement")
+    
+    # print("prediction: ")
+    #print(tester(clf, datas[0][:185000], datas[1][:185000]))
     
     # tree.plot_tree(clf)
     # plt.show()
